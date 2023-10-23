3b6b984cf3dc1e1da36fffcc84474f45db16d8c0
shugo <alive.vb.s2@gmail.com>
2023-10-24 05:47:38 +0900

    #1 練習用のpathやviewを削除

--- backend/web-back/paidholidays/urls.py
+++ backend/web-back/paidholidays/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import DetailPaidHolidays, ListPaidHolidays, get_summary_data
 
 urlpatterns = [
-    path("<int:pk>/", DetailPaidHolidays.as_view()),
     path("", ListPaidHolidays.as_view()),
+    path("<int:pk>/", DetailPaidHolidays.as_view()),
     path("summary/", get_summary_data),
 ]

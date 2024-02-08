41a0e83b38a37c4e5b2723ad2b1114a95dbe00e7
shugo <alive.vb.s2@gmail.com>
2024-02-09 06:04:41 +0900

    #1 deleteロジックの作成完了

--- frontend/web-front/src/pages/index.tsx
+++ frontend/web-front/src/pages/index.tsx
@@ -40,10 +40,15 @@ export default function Home({ api, summary }) {
 				<br />
 
 				<p className="px-2 mb-2 text-gray-500 dark:text-gray-400">年休一覧</p>
-				<button type="submit" onClick={() => router.push('/create')}>
-					create
-				</button>
 				<ContentsList data={api} />
+				<div className="fixed bottom-12 right-16">
+					<button
+						className="bg-rose-700 hover:bg-rose-800 text-gray-300 font-bold py-2 px-4 rounded-full shadow-lg"
+						onClick={() => router.push('/create')}
+					>
+						＋
+					</button>
+				</div>
 			</Section>
 		</div>
 	);

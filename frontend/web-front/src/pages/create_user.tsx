c74b1a9c58c097592dc685eca53a4a35e46f1833
shugo <alive.vb.s2@gmail.com>
2024-02-15 05:55:16 +0900

    #8 コードの整形のみ

--- frontend/web-front/src/pages/create_user.tsx
+++ frontend/web-front/src/pages/create_user.tsx
@@ -4,8 +4,11 @@ import { InputForm } from '../components/elements/InputForm';
 import { CREATE_USER_FORM_ITEMS } from '../const/CREATE_USER_FORM_ITEMS';
 import { ErrorMessage } from '@hookform/error-message';
 import axios from 'axios';
+import { Router, useRouter } from 'next/router';
 
 export default function CreateUser() {
+	const router = useRouter();
+
 	const {
 		register,
 		handleSubmit,
@@ -15,14 +18,22 @@ export default function CreateUser() {
 	} = useForm({ reValidateMode: 'onSubmit' });
 
 	const onSubmit = async (data) => {
-		await axios.post('');
+		await axios
+			.post('http://localhost:8080/api/user/create/', data, {
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8',
+				},
+				withCredentials: true,
+			})
+			.then(() => router.push('/login/'))
+			.catch((e) => alert(e));
 	};
 
 	return (
 		<Section>
 			<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 				<a
-					href="#"
+					onClick={() => router.push('/')}
 					className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
 				>
 					NenQ
@@ -37,6 +48,30 @@ export default function CreateUser() {
 							className="space-y-4 md:space-y-6"
 						>
 							<label
+								htmlFor="username"
+								className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>
+								ユーザー名
+							</label>
+							<input
+								type="username"
+								{...register('username', {
+									required: 'ユーザー名を入力してください',
+									pattern: {
+										value: /.{0,20}$/,
+										message: '20文字以下で入力してください',
+									},
+								})}
+								className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							/>
+							<ErrorMessage
+								errors={errors}
+								name="username"
+								render={({ message }) =>
+									message ? <p className="text-rose-500">{message}</p> : null
+								}
+							/>
+							<label
 								htmlFor="email"
 								className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
 							>
@@ -46,7 +81,6 @@ export default function CreateUser() {
 								type="email"
 								{...register('email', {
 									required: 'メールアドレスを入力してください',
-
 									pattern: {
 										value:
 											/^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
@@ -62,7 +96,6 @@ export default function CreateUser() {
 									message ? <p className="text-rose-500">{message}</p> : null
 								}
 							/>
-
 							<label
 								htmlFor="password"
 								className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"

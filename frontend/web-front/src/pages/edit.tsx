999bc903bfcc9606c89098f856cb27ab06f3c930
shugo <alive.vb.s2@gmail.com>
2024-02-28 06:32:23 +0900

    #14 編集画面上のボタンのスタイルを修正

--- frontend/web-front/src/pages/edit.tsx
+++ frontend/web-front/src/pages/edit.tsx
@@ -78,7 +78,8 @@ export default function Edit() {
 							)}
 							<button
 								type="submit"
-								className="w-full text-white bg-sky-800 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+								disabled={!isDirty || !isValid}
+								className="w-full text-white bg-sky-500 hover:bg-sky-600 focus:ring-4 focus:outline-none disabled:bg-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-700 dark:hover:bg-sky-900 dark:disabled:bg-gray-500"
 							>
 								更新
 							</button>

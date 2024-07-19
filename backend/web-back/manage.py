5d0ddb694c90524e6835dff37da54ba4d3bb2065
shugo <alive.vb.s2@gmail.com>
2024-07-20 08:30:26 +0900

    #33 Cloud Run ジョブを用いて本番環境のmigrateの実行

--- backend/web-back/manage.py
+++ backend/web-back/manage.py
@@ -6,6 +6,7 @@ from os.path import dirname, join
 
 from dotenv import load_dotenv
 
+print(__file__)
 dotenv_path = join(dirname(__file__), "/config/secret/.env")
 load_dotenv(dotenv_path=dotenv_path)
 print(dotenv_path)

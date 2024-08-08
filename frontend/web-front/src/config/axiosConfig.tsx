2557e86ff86f68bfc1bed9d4386e114a0e326196
shugo <alive.vb.s2@gmail.com>
2024-08-08 14:23:49 +0900

    #34 cors_allow_headersでaccess-control-allow-originを設定

--- frontend/web-front/src/config/axiosConfig.tsx
+++ frontend/web-front/src/config/axiosConfig.tsx
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Content-Type'] =
-	'application/json; charset=utf-8';
+	'application/json;charset=utf-8';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] =
+	'https://nenq.vercel.app';
+// axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.withCredentials = true;
 
 export const serverSideAxios = axios.create({

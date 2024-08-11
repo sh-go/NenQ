2562991524fadb452693b3c34fc9328ea554ce95
shugo <alive.vb.s2@gmail.com>
2024-08-12 08:18:08 +0900

    #34 axiosのheaderにAcceptを追加

--- frontend/web-front/src/config/axiosConfig.tsx
+++ frontend/web-front/src/config/axiosConfig.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Content-Type'] =
 	'application/json;charset=utf-8';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] =

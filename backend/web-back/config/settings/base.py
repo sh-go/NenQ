ffb7b517dec3a35c4f913ef34de1a09514bac6ff
shugo <alive.vb.s2@gmail.com>
2024-08-13 08:35:18 +0900

    #34 jwtのaccess_token_lifetimeを修正

--- backend/web-back/config/settings/base.py
+++ backend/web-back/config/settings/base.py
@@ -124,8 +124,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": "Bearer",
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "USER_ID_FIELD": "uuid",
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,

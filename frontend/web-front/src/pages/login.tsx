6fcec8ecb8d50351bcd5e335b592781e107e9af2
shugo <alive.vb.s2@gmail.com>
2024-08-15 15:49:51 +0900

    #34 DjangoでSESSION_COOKIE_DOMAINを設定

--- frontend/web-front/src/pages/login.tsx
+++ frontend/web-front/src/pages/login.tsx
@@ -1,3 +1,4 @@
+import { getCookie } from 'cookies-next';
 import { useRouter } from 'next/router';
 import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
@@ -48,6 +49,8 @@ export default function Login() {
 					isLogin: true,
 					// username: user.username,
 				});
+				console.log(`resの内容：${res.request}`);
+				console.log(getCookie('access_token'));
 				router.push('/');
 			})
 			.catch((e) => {
@@ -72,7 +75,7 @@ export default function Login() {
 				>
 					NenQ
 				</a>
-				<div className="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
+				<div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
 					<div className="space-y-4 p-6 sm:p-8 md:space-y-6">
 						<h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">
 							ログイン

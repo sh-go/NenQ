243d38d1affb6654aa4e4834c8bc23922523f203
shugo <alive.vb.s2@gmail.com>
2024-10-23 06:16:15 +0900

    #39 createページに遷移出来ない事象の確認

--- frontend/web-front/src/pages/create.tsx
+++ frontend/web-front/src/pages/create.tsx
@@ -93,14 +93,15 @@ export default function Create() {
 												name={item.name}
 												render={({ field: { onChange, value, name } }) => (
 													<Datepicker
+														i18n={'ja'}
 														inputId={name}
 														value={value}
 														asSingle={true}
 														useRange={false}
 														onChange={onChange}
-														// i18n={'ja'}
 														required={true}
-														inputClassName="min-w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 dark:border-gray-600 dark:bg-gray-700 dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+														startWeekOn="mon"
+														inputClassName="min-w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 dark:border-gray-600 dark:text-white dark:bg-gray-700 dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
 													/>
 												)}
 											/>

a914b78b0d7b93c245dc055db62ed96dfbec3c79
shugo <alive.vb.s2@gmail.com>
2025-07-23 14:54:06 +0900

    #75 faviconの設定

--- frontend/web-front/src/pages/_document.tsx
+++ frontend/web-front/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="ja">
-			<Head></Head>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<body className="bg-gray-50 text-gray-600 dark:bg-gray-800 dark:text-white">
 				<Main />
 				<NextScript />
--- frontend/web-front/src/pages/index.tsx
+++ frontend/web-front/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSidePropsContext } from 'next';
 import React, { Suspense, useRef, useState } from 'react';
 import { IoIosLogOut } from 'react-icons/io';
 
+import Head from 'next/head';
 import Button from '../components/elements/Button';
 import { ChangeDarkModeButton } from '../components/elements/ChangeDarkModeButton';
 import Section from '../components/layouts/Section';
@@ -56,75 +57,81 @@ export default function Home({
 	const cancelButtonRef = useRef(null);
 
 	return (
-		<div>
-			<div className="mb-4 bg-white px-6 py-3 dark:bg-gray-700">
-				<div className="flex items-center justify-between">
-					<div className="text-3xl">
-						NenQ <span className="text-sm">ー年休管理アプリー</span>
-					</div>
-					<div className="flex gap-3 px-2">
-						<div>
-							<ChangeDarkModeButton />
+		<>
+			<Head>
+				<title>ホーム</title>
+				<meta name="description" content="トップページ説明" />
+			</Head>
+			<div>
+				<div className="mb-4 bg-white px-6 py-3 dark:bg-gray-700">
+					<div className="flex items-center justify-between">
+						<div className="text-3xl">
+							NenQ <span className="text-sm">ー年休管理アプリー</span>
 						</div>
-						<div>
-							<button type="submit" onClick={() => setLogoutOpen(true)}>
-								<IoIosLogOut size="2em" />
+						<div className="flex gap-3 px-2">
+							<div>
+								<ChangeDarkModeButton />
+							</div>
+							<div>
+								<button type="submit" onClick={() => setLogoutOpen(true)}>
+									<IoIosLogOut size="2em" />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<Section>
+					<p className="mb-2 px-2">取得状況</p>
+					<Suspense fallback="<div>loading...</div>">
+						<Summary summaryData={summary} carryOverData={carryOver} />
+					</Suspense>
+					<br />
+					<hr className="border-gray-300" />
+					<br />
+					<div className="flex justify-between">
+						<p className="mb-2 px-2">年休一覧</p>
+						<div className="flex gap-4">
+							<Button
+								rounded
+								className="hidden bg-rose-500 px-4 py-2 text-sm font-bold text-white shadow-lg hover:bg-rose-800 dark:bg-rose-700 dark:hover:bg-rose-800 sm:block"
+								onClick={() => setCreateOpen(true)}
+							>
+								＋　追加
+							</Button>
+							<button
+								className="px-4 py-2 text-sm text-gray-400 hover:text-gray-500"
+								onClick={() => setDeleteAllOpen(true)}
+							>
+								✗　全削除
 							</button>
 						</div>
 					</div>
+					<ContentsList data={api} />
+				</Section>
+				<LogoutModal
+					logoutOpen={logoutOpen}
+					setLogoutOpen={setLogoutOpen}
+					cancelButtonRef={cancelButtonRef}
+				/>
+				<CreateModal
+					createOpen={createOpen}
+					setCreateOpen={setCreateOpen}
+					cancelButtonRef={cancelButtonRef}
+				/>
+				<DeleteAllModal
+					deleteAllOpen={deleteAllOpen}
+					setDeleteAllOpen={setDeleteAllOpen}
+					cancelButtonRef={cancelButtonRef}
+				/>
+				<div>
+					<Button
+						className="fixed bottom-5 right-5 block rounded-full bg-rose-500 px-4 py-2 text-lg font-bold text-white shadow-lg hover:bg-rose-800 dark:bg-rose-700 dark:hover:bg-rose-800 sm:hidden"
+						onClick={() => setCreateOpen(true)}
+					>
+						＋
+					</Button>
 				</div>
 			</div>
-			<Section>
-				<p className="mb-2 px-2">取得状況</p>
-				<Suspense fallback="<div>loading...</div>">
-					<Summary summaryData={summary} carryOverData={carryOver} />
-				</Suspense>
-				<br />
-				<hr className="border-gray-300" />
-				<br />
-				<div className="flex justify-between">
-					<p className="mb-2 px-2">年休一覧</p>
-					<div className="flex gap-4">
-						<Button
-							rounded
-							className="hidden bg-rose-500 px-4 py-2 text-sm font-bold text-white shadow-lg hover:bg-rose-800 dark:bg-rose-700 dark:hover:bg-rose-800 sm:block"
-							onClick={() => setCreateOpen(true)}
-						>
-							＋　追加
-						</Button>
-						<button
-							className="px-4 py-2 text-sm text-gray-400 hover:text-gray-500"
-							onClick={() => setDeleteAllOpen(true)}
-						>
-							✗　全削除
-						</button>
-					</div>
-				</div>
-				<ContentsList data={api} />
-			</Section>
-			<LogoutModal
-				logoutOpen={logoutOpen}
-				setLogoutOpen={setLogoutOpen}
-				cancelButtonRef={cancelButtonRef}
-			/>
-			<CreateModal
-				createOpen={createOpen}
-				setCreateOpen={setCreateOpen}
-				cancelButtonRef={cancelButtonRef}
-			/>
-			<DeleteAllModal
-				deleteAllOpen={deleteAllOpen}
-				setDeleteAllOpen={setDeleteAllOpen}
-				cancelButtonRef={cancelButtonRef}
-			/>
-			<div>
-				<Button
-					className="fixed bottom-5 right-5 block rounded-full bg-rose-500 px-4 py-2 text-lg font-bold text-white shadow-lg hover:bg-rose-800 dark:bg-rose-700 dark:hover:bg-rose-800 sm:hidden"
-					onClick={() => setCreateOpen(true)}
-				>
-					＋
-				</Button>
-			</div>
-		</div>
+		</>
 	);
 }

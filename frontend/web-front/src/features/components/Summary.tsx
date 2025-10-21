90c42bce64bc03d7c0f656e487f6dad963deb5b8
shugo <alive.vb.s2@gmail.com>
2025-10-21 11:20:05 +0900

    #100 グラフチャートをたたき台として挿入

--- frontend/web-front/src/features/components/Summary.tsx
+++ frontend/web-front/src/features/components/Summary.tsx
@@ -1,6 +1,19 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 import { FiEdit } from 'react-icons/fi';
+import {
+	Area,
+	AreaChart,
+	CartesianGrid,
+	Cell,
+	Legend,
+	Pie,
+	PieChart,
+	ResponsiveContainer,
+	Tooltip,
+	XAxis,
+	YAxis,
+} from 'recharts';
 
 type Props = {
 	summaryData: SummaryData;
@@ -13,9 +26,66 @@ export default function Summary({
 }: Props): React.JSX.Element {
 	const router = useRouter();
 
+	const pieChartData = React.useMemo(
+		() => [
+			{ name: '取得済み', value: summaryData.usedDate },
+			{ name: '残り', value: summaryData.remainDate },
+		],
+		[summaryData]
+	);
+	const COLORS = ['#34d399', '#f87171'];
+
+	const areaChartData = React.useMemo(
+		() => [
+			{
+				name: 'Page A',
+				uv: 4000,
+				pv: 2400,
+				amt: 2400,
+			},
+			{
+				name: 'Page B',
+				uv: 3000,
+				pv: 1398,
+				amt: 2210,
+			},
+			{
+				name: 'Page C',
+				uv: 2000,
+				pv: 9800,
+				amt: 2290,
+			},
+			{
+				name: 'Page D',
+				uv: 2780,
+				pv: 3908,
+				amt: 2000,
+			},
+			{
+				name: 'Page E',
+				uv: 1890,
+				pv: 4800,
+				amt: 2181,
+			},
+			{
+				name: 'Page F',
+				uv: 2390,
+				pv: 3800,
+				amt: 2500,
+			},
+			{
+				name: 'Page G',
+				uv: 3490,
+				pv: 4300,
+				amt: 2100,
+			},
+		],
+		[summaryData]
+	);
+
 	return (
 		<div className="flex flex-col">
-			<div className="-m-1.5 overflow-x-auto">
+			<div className="-m-1.5 ">
 				<div className="inline-block min-w-full p-1.5 align-middle">
 					<div className="overflow-hidden rounded-lg border shadow-md dark:border-gray-700">
 						<table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
@@ -99,6 +169,60 @@ export default function Summary({
 						</table>
 					</div>
 				</div>
+			</div>
+			<div className="mt-4 flex justify-center md:mt-0 md:w-1/2">
+				<ResponsiveContainer width="100%" height={250}>
+					<PieChart>
+						<Pie
+							data={pieChartData}
+							dataKey="value"
+							nameKey="name"
+							cx="50%"
+							cy="50%"
+							innerRadius={60}
+							outerRadius={80}
+						>
+							{pieChartData.map((entry, index) => (
+								<Cell
+									key={`cell-${index}`}
+									fill={COLORS[index % COLORS.length]}
+								/>
+							))}
+						</Pie>
+						<Tooltip />
+						<Legend />
+					</PieChart>
+				</ResponsiveContainer>
+			</div>
+			<div className="mt-8 flex justify-center md:mt-0 md:w-1/2">
+				<ResponsiveContainer width="100%" height={400}>
+					<AreaChart
+						style={{
+							width: '100%',
+							maxWidth: '700px',
+							maxHeight: '70vh',
+							aspectRatio: 1.618,
+						}}
+						data={areaChartData}
+						margin={{
+							top: 20,
+							right: 0,
+							left: 0,
+							bottom: 0,
+						}}
+					>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name" />
+						<YAxis width="auto" />
+						<Tooltip />
+						<Area
+							type="monotone"
+							dataKey="uv"
+							stroke="#8884d8"
+							fill="#8884d8"
+						/>
+					</AreaChart>
+				</ResponsiveContainer>
 			</div>
 		</div>
 	);
